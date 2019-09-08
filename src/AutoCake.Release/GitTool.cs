58004402ae9704b3a3cfc8b09070de36b93695e9
moerwald <andreas@mewald.com>
2019-09-08 07:55:17 +0200

    - Added docu. - Now using expressioned bodied members.

--- src/AutoCake.Release/GitTool.cs
+++ src/AutoCake.Release/GitTool.cs
@@ -9,15 +9,22 @@ public class GitTool : Tool<GitToolSettings>
 {
     readonly ICakeLog logger;
 
-    public GitTool(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner,
-        IToolLocator tools, ICakeLog logger)
+    public GitTool(IFileSystem fileSystem,
+                   ICakeEnvironment environment,
+                   IProcessRunner processRunner,
+                   IToolLocator tools,
+                   ICakeLog logger)
         : base(fileSystem, environment, processRunner, tools)
     {
         this.logger = logger;
     }
 
     public GitTool(ICakeContext context)
-        : this(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools, context.Log)
+        : this(context.FileSystem,
+               context.Environment,
+               context.ProcessRunner,
+               context.Tools,
+               context.Log)
     {
     }
 
@@ -42,18 +49,23 @@ public class GitTool : Tool<GitToolSettings>
         return LastExitCode;
     }
 
-    protected override string GetToolName()
-    {
-        return "Git";
-    }
+    /// <summary>
+    /// Called by Tool base class
+    /// </summary>
+    /// <returns></returns>
+    protected override string GetToolName() => "Git"; 
 
-    protected override IEnumerable<string> GetToolExecutableNames()
-    {
-        return new List<string> {"Git.exe", "git.exe"};
-    }
+    /// <summary>
+    /// Called by Tool base class to retrieve the executables that should be invoked with the given arguments
+    /// when calling <see cref="Run"/>.
+    /// </summary>
+    /// <returns></returns>
+    protected override IEnumerable<string> GetToolExecutableNames() => new List<string> {"Git.exe", "git.exe"};
 
-    protected override void ProcessExitCode(int exitCode)
-    {
-        LastExitCode = exitCode;
-    }
+    /// <summary>
+    /// Called by Tool base class the inject the exit code of the executable retrieved via <see cref="GetToolExecutableNames"/>.
+    /// </summary>
+    /// <param name="exitCode"></param>
+    protected override void ProcessExitCode(int exitCode) => LastExitCode = exitCode; 
+
 }
